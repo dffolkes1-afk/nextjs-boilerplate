e2cf5e418e304779d3a3fd6b4b8b3c0a504a0ed8
dwayne ffolkes <dffolkes.1@gmail.com>
2026-01-08 18:34:00 -0500

    Create SchoolNode component with basic layout Add School Node education system demo page

---
+++ app/games/node-runner/app/games/value-split/app/games/school-node/page.tsx
@@ -0,0 +1,8 @@
+export default function SchoolNode() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "sans-serif" }}>
+      <h1>School Node</h1>
+      <p>Education-sponsored node operating inside the system.</p>
+    </main>
+  );
+}
